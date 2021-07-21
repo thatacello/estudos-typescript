42e530bfe4a77161fdceb77465f07df0823ea87b
tsci <tsci@gft.com>
2021-07-21 13:50:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    // void não retorna nenhum valor, apenas executa uma ação
+    // function alertUser(): void {
+    //     alert("This is an alert message!");
+    // }
+    // alertUser();
+    var number1 = 12;
+    var number2 = 12;
+    if (number1 == number2) {
+        alert("number1 == number2");
+    }
+    else {
+        alert("number1 is not iqual number2");
+    }
+};
---
+++ app.ts
@@ -0,0 +1,21 @@
+window.onload = () => {
+    // void não retorna nenhum valor, apenas executa uma ação
+    // function alertUser(): void {
+    //     alert("This is an alert message!");
+    // }
+    // alertUser();
+
+    // if/else
+    // var number1: number = 12;
+    // var number2: number = 12;
+
+    // if(number1 == number2){
+    //     alert("number1 == number2");
+    // }
+    // else{
+    //     alert("number1 is not iqual number2");
+    // }
+
+    // while/do while 
+    
+};
